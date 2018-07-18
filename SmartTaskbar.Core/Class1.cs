f63c4622cfd6e2fc8ff87d2f0168479b806181d3
Chanple <chanple@outlook.com>
2018-07-18 15:55:23 +0800

    ~

---
+++ SmartTaskbar.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTaskbar.Core
+{
+    public class Class1
+    {
+    }
+}
--- SmartTaskbar/MsgFilter.cs
+++ SmartTaskbar/MsgFilter.cs
@@ -9,13 +9,13 @@ namespace SmartTaskbar
             switch (m.Msg)
             {
                 case SafeNativeMethods.MSG_INSTANCE:
-                    //MessageBox.Show("!");
+
                     return true;
                 case SafeNativeMethods.MSG_MAX:
-                    MessageBox.Show("What");
+                    
                     return true;
                 case SafeNativeMethods.MSG_UNMAX:
-                    MessageBox.Show("Now");
+                    
                     return true;
                 default:
                     return false;
